e58d6396edfddd0c8e16a69474049d9504c91730
Yar Ffullex <72185613+Ffullex@users.noreply.github.com>
2021-01-20 23:18:41 +0700

    add main files

---
+++ Matrix.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class Matrix
+    {
+        private int col;
+        private int str;
+        public int[,] matset;
+        public int Column
+        {
+            get { return col; }                  // Считывание элемента
+            set { if (value > 0) col = value; }  // Запись значения
+        }
+        public int String
+        {
+            get { return str; }                  // Считывание элемента
+            set { if (value > 0) str = value; }  // Запись значения
+        }
+        // Конструктор матрицы
+        public Matrix(int col, int str)
+        {
+            this.col = col;
+            this.str = str;
+            matset = new int[this.col, this.str];
+        }
+        // Индексатор
+        public int this[int i, int j]
+        {
+            get
+            {
+               return matset[i, j];
+            }
+            set
+            {
+                matset[i, j] = value;
+            }
+        }
+
+        public void PushInMat()
+        {
+            for (int j = 0; j < str; j++)
+            {
+                for (int i = 0; i < col; i++)
+                {
+
+                    matset[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+        public void PushOutMat()
+        {
+            Console.WriteLine("Rоличество столбцов: " + col);
+            Console.WriteLine("Rоличество строк" + str);
+            Console.WriteLine("Заполните вашу матрицу");
+            for (int j = 0; j < str; j++)
+            {
+                for (int i = 0; i < col; i++)
+                {
+
+                    Console.Write(matset[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static Matrix summer(Matrix a, Matrix b)
+        {
+            Matrix resultSum = new Matrix(a.col, a.str);
+            for (int i = 0; i < a.str; i++)
+            {
+                for (int j = 0; j < a.col; j++)
+                {
+                    resultSum[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return resultSum;
+        }
+        // Перегрузка сложения
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            return Matrix.summer(a, b);
+        }
+
+        public static Matrix substractor(Matrix a, Matrix b)
+        {
+            Matrix resultSub = new Matrix(a.str, b.col);
+            for (int i = 0; i < a.str; i++)
+            {
+                for (int j = 0; j < b.col; j++)
+                {
+                    resultSub[i, j] = a[i, j] - b[i, j];
+                }
+            }
+            return resultSub;
+        }
+        // Перегрузка оператора вычитания
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            return Matrix.substractor(a, b);
+        }
+        public static Matrix multiplicator1(Matrix a, Matrix b)
+        {
+            Matrix resultMul = new Matrix(a.str, b.col);
+            for(int i = 0; i < a.str; i++)
+            {
+                for (int j = 0; j < b.col; j++)
+                {
+                    resultMul[i, j] = 0;
+                    for (int k = 0; k < a.col; k++)
+                    {
+                        resultMul[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return resultMul;
+        }
+
+        // перегрузка оператора умножения
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            return Matrix.multiplicator1(a, b);
+        }
+
+        // Деструктор 
+        ~Matrix()
+        {
+            Console.WriteLine("Очистка");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Matrix
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность матрицы ");
+            Console.WriteLine("Введите количество столбцов: ");
+            int col1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество строк");
+            int str1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Заполните матрицу");
+
+
+            Matrix Mat1 = new Matrix(col1, str1);
+            Mat1.PushInMat();
+            Mat1.PushOutMat();
+            Console.WriteLine();
+
+            Console.WriteLine("Введите размерность матрицы ");
+            Console.WriteLine("Введите количество столбцов: ");
+            int col2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество строк");
+            int str2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Заполните матрицу");
+
+            Matrix Mat2 = new Matrix(col2, str2);
+            Mat2.PushInMat();
+            Mat2.PushOutMat();
+            Console.WriteLine();
+
+            if ((col1 == col2) && (str1 == str2)) {
+                Console.WriteLine("Вы можете сложить либо вычесть  данные матрицы либо перемножить данные матрицы");
+                Console.WriteLine("Если вы хотите их \n Сложить, нажмите 1. \n Вычесть, нажмите 2. \n Перемножить, нажмите 3");
+                int press = Convert.ToInt32(Console.ReadLine());
+                Matrix Mat3 = new Matrix(str1, col2);
+                switch (press)
+                {
+                    case 1:
+                        Mat3 = Mat1 + Mat2;
+                        break;
+                    case 2:
+                        Mat3 = Mat1 - Mat2;
+                        break;
+                    case 3:
+                        Mat3 = Mat1 * Mat2;
+                        break;
+                }
+                Mat3.PushOutMat();
+            }
+
+            /*            else if (str1 == col2)
+                        {
+                            Console.WriteLine("Вы можете только перемножить данные матрицы. \n");
+                            Matrix Mat4 = new Matrix(str1, col2);
+                            Mat4 = Mat1 * Mat2;
+                            Mat4.PushOutMat();
+                        }*/
+            else
+            {
+                Console.WriteLine("Матрицы разные, как... в общем, рекомендуется использовать одноразмерные матрицы. ");
+            }
+
+        }
+    }
+}
